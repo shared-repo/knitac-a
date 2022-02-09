ea81e58087e2b63e80c64ffe8b0565f88ea93c29
instructor <instructor@example.com>
2022-02-09 11:04:29 +0900

    update code

--- workspace/python-basic/g_app.py
+++ workspace/python-basic/g_app.py
@@ -1,6 +1,36 @@
-import g_mod1
+# [ 모듈 사용 1 ]
 
-print( g_mod1.name ) # 모듈의 변수 사용
+# import g_mod1
 
-result = g_mod1.add(11, 24, 37, 46, 25)
+# print( g_mod1.name ) # 모듈의 변수 사용
+
+# result = g_mod1.add(11, 24, 37, 46, 25)
+# print( result )
+
+# [ 모듈 사용 2 ]
+# import sys
+
+# print( sys.path )
+
+# [ 모듈 사용 3 ]
+
+# from g_mod1 import name, add
+
+# print( name )
+# print( add(10, 20, 30, 40, 50) )
+
+# [ 모듈 사용 4 ]
+
+# from g_mod1 import *
+
+# print( name )
+# print( add(10, 20, 30, 40, 50) )
+
+# [ 모듈 사용 5 ]
+
+import g_mod1 as my_mod
+
+print( my_mod.name ) # 모듈의 변수 사용
+
+result = my_mod.add(11, 24, 37, 46, 25)
 print( result )
--- workspace/python-basic/g_mod1.py
+++ workspace/python-basic/g_mod1.py
@@ -6,3 +6,8 @@ def add(*numbers):
         total += number
 
     return total
+
+# 테스트 코드 ( import일 때에는 실행되지 않도록 설정 필요 )
+if __name__ == "__main__": # python g_mod1.py로 실행한 경우
+    print( "add function test ")
+    print( add(10, 20, 30, 40, 50) )
