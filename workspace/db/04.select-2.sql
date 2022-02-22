4e8754aba0d94753189810667571ee6f4669b525
instructor <instructor@example.com>
2022-02-22 12:17:26 +0900

    update code

--- workspace/db/04.select-2.sql
+++ workspace/db/04.select-2.sql
@@ -69,6 +69,50 @@ ORDER BY c.name;
 SELECT *
 FROM orders;
 
+SELECT o.orderid, c.custid, c.name, b.bookid, b.bookname, o.saleprice, o.orderdate
+FROM orders o, customer c, book b
+WHERE o.custid = c.custid AND o.bookid = b.bookid;
+
+SELECT o.orderid, c.custid, c.name, b.bookid, b.bookname, o.saleprice, o.orderdate
+FROM orders o
+INNER JOIN customer c
+ON o.custid = c.custid
+INNER JOIN book b
+ON o.bookid = b.bookid;
+
+-- 가격이 20,000원인 도서를 주문한 고객의 이름과 도서의 이름을 구하시오 ( JOIN + WHERE )
+SELECT o.orderid, c.custid, c.name, b.bookid, b.bookname, o.saleprice, o.orderdate
+FROM orders o, customer c, book b
+WHERE o.custid = c.custid 
+	  AND o.bookid = b.bookid
+      AND b.price = 20000;
+
+SELECT o.orderid, c.custid, c.name, b.bookid, b.bookname, o.saleprice, o.orderdate
+FROM orders o
+INNER JOIN customer c
+ON o.custid = c.custid
+INNER JOIN book b
+ON o.bookid = b.bookid
+WHERE b.price = 20000;
+
+-- 도서를 구매하지 않은 고객을 포함하여 고객의 이름과 고객이 주문한 도서의 판매가격을 구하시오.
+-- ( 고객별 주문 내역 조회 )
+SELECT c.name, b.bookname, o.orderid, o.saleprice, o.orderdate
+FROM customer c 
+LEFT OUTER JOIN orders o -- 왼쪽 컬럼 (customer)는 모두 조회하고 오른쪽 컬럼은 일치하는 것만 조회
+ON o.custid = c.custid
+LEFT OUTER JOIN book b
+ON o.bookid = b.bookid
+ORDER BY c.name;
+
+-- 고객별 주문 실적 ( 고객별 주문 총액 조회 )
+SELECT c.custid, c.name, SUM(o.saleprice) 주문총액
+FROM customer c
+LEFT OUTER JOIN orders o
+ON c.custid = o.custid
+GROUP BY c.custid
+ORDER BY SUM(o.saleprice) DESC;
+
 
 
 
