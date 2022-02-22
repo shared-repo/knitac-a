c60e90840cc3e0c48befb60d85b945158916df54
instructor <instructor@example.com>
2022-02-22 14:58:35 +0900

    update code

--- workspace/db/04.select-2.sql
+++ workspace/db/04.select-2.sql
@@ -129,9 +129,95 @@ FROM book
 WHERE price = (	SELECT MAX(price)
 				FROM book );
 
+-- 도서를 구매한 적이 있는 고객의 이름을 검색하시오.
+/* 1 */
+SELECT DISTINCT c.name
+FROM customer c, orders o -- inner join : 두 테이블에 모두 있는 일치하는 데이터 조회
+WHERE c.custid = o.custid;
 
+/* 2-1 */
+SELECT DISTINCT custid -- 조회 결과 : 1, 2, 3, 4
+FROM orders;
+/* 2-2 */
+SELECT name
+FROM customer
+WHERE custid IN (1, 2, 3, 4); -- 위에서 조회한 주문 테이블의 고객아이디 적용
 
+/* 3 */
+SELECT name
+FROM customer
+WHERE custid IN ( SELECT DISTINCT custid
+				  FROM orders );
+                  
+-- 대한미디어에서 출판한 도서(book table)를 구매한 고객(orders table)의 이름(customer)을 보이시오
+/* 1 */
+SELECT c.name
+FROM customer c, orders o, book b
+WHERE c.custid = o.custid AND o.bookid = b.bookid
+	  AND b.publisher = '대한미디어';
 
+/* 2-1 */
+SELECT bookid	-- 조회결과 : 3, 4
+FROM BOOK
+WHERE publisher = '대한미디어';
+/* 2-2 */
+SELECT custid	-- 조회결과 : 1
+FROM orders
+WHERE bookid IN (3, 4); -- 위에서 조회한 bookid를 적용
+/* 2-3 */
+SELECT name
+FROM customer
+WHERE custid IN (1); 	-- 위에서 조회한 custid를 적용
 
+/* 3 */
+SELECT name
+FROM customer
+WHERE custid IN ( SELECT custid
+				  FROM orders
+				  WHERE bookid IN ( SELECT bookid
+									FROM BOOK
+									WHERE publisher = '대한미디어') );
+                                    
+-- 출판사별로 출판사의 평균 도서 가격보다 비싼 도서를 구하시오
+/* 1-1 : 굿스포츠 출판사의 평균 도서 가격 */
+SELECT AVG(price) 평균가격 -- 조회 결과 : 7000
+FROM book
+WHERE publisher = '굿스포츠';
+/* 1-2 : 굿스포츠 출판사의 도서 조회 */
+SELECT *
+FROM book
+WHERE publisher = '굿스포츠';
+/* 1-3 : 굿스포츠 출판사의 도서 중 1-1의 가격보다 비싼 도서 조회 */
+SELECT *
+FROM book
+WHERE publisher = '굿스포츠' AND price > 7000;
+
+/* 2 : 1과 3을 결합해서 서브쿼리 작성 */
+SELECT *
+FROM book
+WHERE publisher = '굿스포츠' AND price > ( SELECT AVG(price)
+										 FROM book
+										 WHERE publisher = '굿스포츠' );
+                                         
+/* 3 : 2의 작업을 모든 도서에 적용 */
+SELECT mb.*
+FROM book mb
+WHERE mb.price > ( SELECT AVG(sb.price)
+				   FROM book sb
+				   -- WHERE sb.publisher = '현재 메인쿼리의 publisher' );
+                   WHERE sb.publisher = mb.publisher );
+                   
+-- 주문이 있는 고객의 이름과 주소를 보이시오
+SELECT *
+FROM customer c
+WHERE c.custid IN ( SELECT o.custid
+				    FROM orders o
+				    WHERE o.custid = c.custid );
+                   
+SELECT *
+FROM customer c
+WHERE EXISTS ( SELECT o.custid -- EXISTS : 존재 여부만 확인 (비교하지 않습니다)
+			   FROM orders o
+			   WHERE o.custid = c.custid );
 
 
