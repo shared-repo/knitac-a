9aa8325a84e77b49cf35dd442dd065a784c6dab8
instructor <instructor@example.com>
2022-03-07 12:20:26 +0900

    update code

--- workspace/django-basic/mysite/pybo/views.py
+++ workspace/django-basic/mysite/pybo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
 
 # from pybo.models import Question
 from .models import Answer, Question
@@ -8,21 +9,13 @@ from .forms import AnswerForm, QuestionForm
 # Create your views here.
 
 def index(request):
-    # 테스트 1 : 단순 문자열 반환
-    # return HttpResponse("Hello, Django Web Application Programming World !!!")
-    
-    # 테스트 2 : 동적 문자열 반환 (매 요청마다 다른 데이터를 반환 - 여기서는 시간 )
-    # from datetime import datetime
-    # now = datetime.now()
-    # # strftime : 지정된 서식에 따라 날짜를 문자열로 변환
-    # time_str = now.strftime("%Y-%m-%d %H:%M:%S") 
-    
-    # return HttpResponse(time_str) # HttpResponse : 응답 전송하는 객체
-
-    # 테스트 3 : HTML 반환 - 템플릿 사용
-    # questions = Question.objects.all() # 데이터베이스의 데이터 조회 ( ORM 자동화 기법 사용 )
     questions = Question.objects.order_by('-create_date') # 데이터베이스의 데이터 조회 ( ORM 자동화 기법 사용 )
-    context = { "questions": questions }
+    
+    page = request.GET.get('page', '1') # ?page=x에서 x의 값 읽기. 없으면 1반환
+    paginator = Paginator(questions, 10) # 페이징 관리자 만들기
+    page_object = paginator.get_page(page) # 현재 페이지 요청
+    
+    context = { "questions": page_object }
     return render(request, 
                   "pybo/question_list.html", 
                   context) # pybo/question_list.html 을 사용해서 응답 html을 반환하는 명령
