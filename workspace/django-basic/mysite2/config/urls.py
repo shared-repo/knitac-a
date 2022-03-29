ed4f619350a83de11b8de20a30506b113bf7330e
instructor <instructor@example.com>
2022-03-29 11:21:21 +0900

    update code

--- workspace/django-basic/mysite2/config/urls.py
+++ workspace/django-basic/mysite2/config/urls.py
@@ -30,4 +30,8 @@ urlpatterns = [
     # path('pybo/', pybo_views.index), # http://.../pybo 요청이 오면 views.index 호출
     path('pybo/', include('pybo.urls')), # http://.../pybo 관련 요청이 오면 pybo.urls.py의 설정 내용 사용
     path('common/', include('common.urls')), # http://.../common 관련 요청이 오면 common.urls.py의 설정 내용 사용
+
+    path('ajax-form/', views.ajax_form, name='ajax_form'),
+    path('ajax-data/', views.ajax_data, name='ajax_data'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # static으로 등록되는 경로 추가 ( 직접 접근 가능 경로 )
--- workspace/django-basic/mysite2/config/views.py
+++ workspace/django-basic/mysite2/config/views.py
@@ -1,6 +1,21 @@
 
+from time import strftime
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'home.html')
+
+def ajax_form(request):
+    return render(request, 'ajax-form.html')
+
+def ajax_data(request):
+    import time
+    time.sleep(5)
+
+    from datetime import datetime 
+    now = datetime.now()
+    time_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    return HttpResponse(time_string)    
